acc61f242cb29b42fb5032babe51bca671f994eb
Tyrsius <tyrsius@gmail.com>
2014-06-01 16:14:46 -0700

    finishing sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -4,8 +4,17 @@
 		var self = this;
 		self.subtotal = ko.observable(20);
 		self.tax = ko.observable(0.05);
-		self.total = ko.computed(function() {
-			return self.subtotal() * (1 + self.tax());
+		self.total = ko.computed({
+			write: function(newValue) {
+				var tax = parseFloat(self.tax()),
+					totalMinusTax = newValue / (1 + tax);
+				self.subtotal(totalMinusTax);
+			},
+			read: function() {
+				var subtotal = parseFloat(self.subtotal()),
+					tax = parseFloat(self.tax());
+				return subtotal * (1 + tax);
+			}
 		});
 	};
 
