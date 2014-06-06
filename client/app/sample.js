f6273532f3b185d46e5f30782f7aacc9fe96885a
tyrsius <tyrsius@gmail.com>
2014-06-05 20:56:24 -0700

    adding node modules and sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,31 +1,14 @@
 (function(app, $, ko) {
 
-	var Person = function(data) {
-		var self = this;
-		self.name = data.name;
-		self.age = data.age;
-		self.location = data.location;
-		self.favoriteColor = data.favoriteColor;
-	};
-
 	var BindingSample = function() {
 		var self = this;
 
-		self.father = new Person({
-			name: 'Curt Ford',
-			age: 50,
-			location: 'Fairview',
-			favoriteColor: 'Blue'
-		});
-
-		self.editing = ko.observable(false);
-		self.toggleEdit = function() {
-			self.editing(!self.editing());
+		self.isAdmin = ko.observable(false);
+		self.toggleAdmin = function() {
+			self.isAdmin(!self.isAdmin());
 		};
 
-		self.template = ko.computed(function() {
-			return self.editing() ? 'editTemplate' : 'viewTemplate';
-		});
+		self.navigationItems = ko.observableArray(['People', 'Locations' , 'Users']);
 	};
 	
 	$(document).ready(function() {
