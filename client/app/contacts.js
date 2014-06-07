8346b7755dbc4b69cffdc0afec4557320c26ecab
Tyrsius <tyrsius@gmail.com>
2014-06-06 22:05:00 -0700

    updating

--- client/app/contacts.js
+++ client/app/contacts.js
@@ -8,6 +8,28 @@
 		self.phoneNumber = ko.observable(data.phoneNumber || '');
 	};
 
+	app.ContactsPageViewmodel = function(init) {
+		var self = this,
+			data = init || {};
+
+		self.contacts = ko.observableArray(data.map(function(c) {
+			return new app.Contact(c);
+		}));
+
+		self.entryContact = ko.observable(new app.Contact());
+
+		self.saveEntry = function() {
+			self.contacts.push(self.entryContact());
+			self.entryContact(new app.Contact());
+		};
+		self.clearEntry = function() {
+			self.entryContact(new app.Contact());
+		};
+		self.removeContact = function(contact) {
+			self.contacts.remove(contact);
+		};
+	};
+
 	$(document).ready(function() {
 		ko.applyBindings(new app.ContactsPageViewmodel(mockData));
 	});
