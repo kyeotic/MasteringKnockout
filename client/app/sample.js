4227261c8476ffe4a50f2301a41ac764c9465648
Tyrsius <tyrsius@gmail.com>
2014-06-18 22:53:30 -0700

    adding sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,29 +1,57 @@
 (function(app, $, ko) {
 
-	ko.bindingHandlers.labelInput = {
+	var defaultChartOptions = { 
+		height: 300, 
+		width: 300,
+		colors: ["#F7464A", "#E2EAE9", "#D4CCC5", "#949FB1", "#4D5360"],
+		animation: false
+	};
+
+	ko.bindingHandlers.doughnutChart = {
 	    init: function(element, valueAccessor) {
-	        var input = document.createElement('input'),
-	        	label = document.createElement('label'),
-	        	labelText = valueAccessor().label,
-	        	inputValue = valueAccessor().value;
+	        var canvas = document.createElement('canvas'),
+	        	options = ko.utils.extend(defaultChartOptions, valueAccessor());
 
-	        label.innerHTML = labelText;
-	        label.appendChild(input);
+	        
+	        element.appendChild(canvas);        
 
-			element.appendChild(label);
+	        ko.computed(function() {
 
-			ko.applyBindingsToNode(input, {
-				value: inputValue,
-				valueUpdate: 'afterkeydown'
-			});
+	        	canvas.height = ko.unwrap(options.height);
+	        	canvas.width = ko.unwrap(options.width);
+
+	        	var chartContext = canvas.getContext('2d'),
+	        		colors = ko.unwrap(options.colors);
+
+	        	var data = ko.toJS(options.data).map(function(x) {
+	        		return {
+	        			value: parseFloat(x.value),
+	        			color: x.color.indexOf('#') === 0 ? "#" + x.color : x.color
+	        		}
+	        	});
+
+	        	new Chart(chartContext).Doughnut(data, options);
+	        }, null, {disposeWhenNodeIsRemoved: element});
 	    }
+	};
+
+	var ChartDatum = function(value, color) {
+		var self = this;
+		self.value = ko.observable(value);
+		self.color = ko.observable(color);
 	};
 
 
 	var BindingSample = function() {
 		var self = this;
 
-		self.name = ko.observable('Timothy');
+		self.chartSeries = [
+			new ChartDatum(20, 'D97041'),
+			new ChartDatum(8, 'C7604C'),
+			new ChartDatum(80, '21323D'),
+			new ChartDatum(13, '9D9B7F'),
+			new ChartDatum(145, '7D4F6D')
+		];
 	};
 	
 	$(document).ready(function() {
