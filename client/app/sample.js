eedfc78e2f0cf2c2d927ca5fc8169582d01203af
Tyrsius <tyrsius@gmail.com>
2014-06-21 14:29:19 -0700

    adding sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,29 +1,16 @@
 (function(app, $, ko) {
 
-	var listTemplate = '<ul class="list-unstyled" data-bind="foreach: items"><li data-bind="text: $data"></li></ul>';
-
-	ko.bindingHandlers.list = {
+	ko.bindingHandlers.stopBinding = {
 	    init: function(element, valueAccessor) {
-	        
-	        element.innerHTML = listTemplate;
-
-        	ko.applyBindingsToDescendants({ items: valueAccessor()}, element);
-
-	        // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
-        	return { controlsDescendantBindings: true };
+	        return { controlsDescendantBindings: ko.unwrap(valueAccessor()) };
 	    }
 	};
-
+	ko.virtualElements.allowedBindings.stopBinding = true;
 
 	var BindingSample = function() {
 		var self = this;
 
-		self.inputs = ko.observableArray(['One', 'Two', 'Three']);
-		self.entry = ko.observable('');
-		self.addEntry = function() {
-			self.inputs.push(self.entry());
-			self.entry('');
-		};
+		self.title = ko.observable('Example');
 	};
 	
 	$(document).ready(function() {
