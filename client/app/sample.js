6f86eeb3cb05d231cead065aefdb95442eb2fcd2
Tyrsius <tyrsius@gmail.com>
2014-06-21 17:10:25 -0700

    testing broken things

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,13 +1,38 @@
 (function(app, $, ko) {
 
-	var listTemplate = '<ul class="list-unstyled" data-bind="foreach: items"><li data-bind="text: $data"></li></ul>';
+	var treeTemplate = 'Name: <span data-bind="text:name"></span><br>'
+	+'Root: <span data-bind="text: isRoot ? \'Self\' : $parentContext.name"></span><br>'
+	+'<ul data-bind="foreach: children">'
+		+'<li data-bind="tree: { data: $data, children: $parentContext.__children, name: $parentContext.__name, isRoot: false }></li>'
+	+'</ul>';
 
-	ko.bindingHandlers.list = {
+	ko.bindingHandlers.stopBinding = {
 	    init: function(element, valueAccessor) {
-	        
-	        element.innerHTML = listTemplate;
+	        return { controlsDescendantBindings: ko.unwrap(valueAccessor()) };
+	    }
+	};
+	ko.virtualElements.allowedBindings.stopBinding = true;
 
-        	ko.applyBindingsToDescendants({ items: valueAccessor()}, element);
+	ko.bindingHandlers.tree = {
+	    init: function(element, valueAccessor, allBindings, viewmodel, bindingContext) {
+	        
+	        var value = valueAccessor();
+	        var context = { 
+	        		__name: value.name,
+	        		__children: value.children,
+	        		//Default to true since template specifies
+	        		isRoot: value.isRoot === undefined || value.isroot,
+	        		name: value.data[value.name],
+	        		children: value.data[value.children],
+	        	};
+
+	        if (!context.isRoot) {
+	        	context = bindingContext.createChildContext(context);
+	        }
+
+	        element.innerHTML = treeTemplate;
+
+        	ko.applyBindingsToDescendants(context, element);
 
 	        // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
         	return { controlsDescendantBindings: true };
@@ -18,11 +43,49 @@
 	var BindingSample = function() {
 		var self = this;
 
-		self.inputs = ko.observableArray(['One', 'Two', 'Three']);
-		self.entry = ko.observable('');
-		self.addEntry = function() {
-			self.inputs.push(self.entry());
-			self.entry('');
+		self.person = { 
+			fullName: 'Alexander Hamilton',
+			descendants: [
+				{
+					fullName: 'James Hamilton',
+					descendants: [
+						{
+							fullName: 'Alexander Hamilton',
+							descendants: [
+								{
+									fullName: 'John Hamilton',
+									descendants: []
+								}, {
+									fullName: 'Rebecca Cunningham',
+									descendants: []
+								}
+							]
+						}, {
+							fullName: 'Elizabeth Pollock',
+							descendants: [
+								{
+									fullName: 'Robert Pollock',
+									descendants: []
+								}, {
+									fullName: 'Annabel Stewart',
+									descendants: []
+								}
+							]
+						}
+					]
+				}, {
+					fullName: 'Rachel Fawcett',
+					descendants: [
+						{
+							fullName: 'Jean Faucette IV',
+							descendants: []
+						}, {
+							fullName: 'Mary Uppington',
+							descendants: []
+						}
+					]
+				}
+			]
 		};
 	};
 	
