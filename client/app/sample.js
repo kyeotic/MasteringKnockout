456242fe7eaea7504bfc83f20f3e8034a54da8ed
Tyrsius <tyrsius@gmail.com>
2014-06-28 11:08:46 -0700

    adding sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,29 +1,13 @@
 (function(app, $, ko) {
 
-	ko.bindingHandlers.slideVisible = {
-	    init: function(element, valueAccessor) {
-	        var value = ko.unwrap(valueAccessor());
-	        $(element).toggle(value);
-	    },
-	    update: function(element, valueAccessor, allBindings) {
-	        var value = ko.unwrap(valueAccessor());
-	        var duration = allBindings.get('slideDuration') || 400;
-
-	        if (value == true)
-	            $(element).slideDown(duration);
-	        else
-	            $(element).slideUp(duration);
-	    }
+	ko.bindingHandlers.text.preprocess = function(value) {
+	    return 'ko.unwrap(' + value + ').toUpperCase()';
 	};
-
 
 	var BindingSample = function() {
 		var self = this;
 
-		self.isShowing = ko.observable(true);
-		self.toggleShowing = function() {
-			self.isShowing(!self.isShowing());
-		}
+		self.firstName = ko.observable('Timothy');
 	};
 	
 	$(document).ready(function() {
