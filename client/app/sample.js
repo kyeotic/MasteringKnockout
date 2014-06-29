2559bc9126e37fe3e61a6b2863237ae705b8ba9a
Tyrsius <tyrsius@gmail.com>
2014-06-29 13:23:34 -0700

    adding sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,14 +1,29 @@
 (function(app, $, ko) {
 
+	ko.bindingHandlers.tooltip = {
+	    getNamespacedHandler: function(binding) {
+	        return {	        	
+	            update: function(element, valueAccessor) {
+	            	//Cleanup previous tooltips
+	            	if (element.attributes && element.attributes['data-original-title'])
+	            		$(element).tooltip('destroy');
+	                $(element).tooltip({ placement: binding, title: ko.unwrap(valueAccessor()) });
+	            }
+	        };
+	    }
+	};
+
 	var BindingSample = function() {
 		var self = this;
 
-		self.firstName = ko.observable('Timothy');
-		self.lastName = ko.observable('Moran');
-		self.birthDate = ko.observable('3/19/1987');
+		self.leftTitle = ko.observable('I\'m on the left');
+		self.rightTitle = ko.observable('I\'m on the right');
+		self.topTitle = ko.observable('I\'m on the top');
+		self.bottomTitle = ko.observable('I\'m on the bottom');
 	};
 	
 	$(document).ready(function() {
+		ko.punches.enableAll();
 		ko.applyBindings(new BindingSample());
 	});
 
