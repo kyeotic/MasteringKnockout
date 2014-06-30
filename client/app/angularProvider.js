79034f2773bdd1e189f0bbe148c5a54925d3de2c
Tyrsius <tyrsius@gmail.com>
2014-06-29 17:02:39 -0700

    reorg code

--- client/app/angularProvider.js
+++ client/app/angularProvider.js
@@ -1,17 +1,5 @@
 (function(app, $, ko) {
 
-var expressionRegex = /{{([\s\S]+?)}}/g;
-function parseAttributeExpression(node, attributeName) {
-    expressionRegex.lastIndex = 0;
-    var attribute = node.getAttribute(attributeName),
-        match = expressionRegex.exec(attribute);
-    return match ? match[1] : null;
-}
-
-function nodeHasAttribute(node, attributeName) {
-    return node.attributes && node.hasAttribute(attributeName);
-}
-
 var directives = [
     { name: 'ng-model', binding: 'value', adds: "valueUpdate: 'afterkeydown'" },
     { name: 'ng-bind', binding: 'text' }
@@ -19,6 +7,18 @@ var directives = [
 
 ko.angularBindingProvider = function() {
   var defaultBindingProvider = new ko.bindingProvider();
+
+    var expressionRegex = /{{([\s\S]+?)}}/g;
+    function parseAttributeExpression(node, attributeName) {
+        expressionRegex.lastIndex = 0;
+        var attribute = node.getAttribute(attributeName),
+            match = expressionRegex.exec(attribute);
+        return match ? match[1] : null;
+    }
+
+    function nodeHasAttribute(node, attributeName) {
+        return node.attributes && node.hasAttribute(attributeName);
+    }
 
     function getBindingsString(node) {
         if (!node.attributes) {
@@ -35,25 +35,26 @@ ko.angularBindingProvider = function() {
             return result;
         }).join(',');
     }
- 
-  this.nodeHasBindings = function(node) {
-    return defaultBindingProvider.nodeHasBindings(node)
-        || directives.some(function(d) { return nodeHasAttribute(node, d.name) });
-  };
-  this.getBindings = function(node, bindingContext) {
-    var bindingsString = defaultBindingProvider.getBindingsString(node, bindingContext) 
-        || getBindingsString(node);
-    return bindingsString ? 
-        defaultBindingProvider.parseBindingsString(bindingsString, bindingContext, node) : 
-        null;
-  };
-  this.getBindingAccessors = function(node, bindingContext) {
-    var bindingsString = defaultBindingProvider.getBindingsString(node, bindingContext)
-        || getBindingsString(node);
-    return bindingsString ? 
-        defaultBindingProvider.parseBindingsString(bindingsString, bindingContext, node, {'valueAccessors':true}) : 
-        null;
-  };
+
+    //Publid methods
+    this.nodeHasBindings = function(node) {
+        return defaultBindingProvider.nodeHasBindings(node)
+            || directives.some(function(d) { return nodeHasAttribute(node, d.name) });
+    };
+    this.getBindings = function(node, bindingContext) {
+        var bindingsString = defaultBindingProvider.getBindingsString(node, bindingContext) 
+            || getBindingsString(node);
+        return bindingsString ? 
+            defaultBindingProvider.parseBindingsString(bindingsString, bindingContext, node) : 
+            null;
+    };
+    this.getBindingAccessors = function(node, bindingContext) {
+        var bindingsString = defaultBindingProvider.getBindingsString(node, bindingContext)
+            || getBindingsString(node);
+        return bindingsString ? 
+            defaultBindingProvider.parseBindingsString(bindingsString, bindingContext, node, {'valueAccessors':true}) : 
+            null;
+    };
 };
 ko.bindingProvider.instance = new ko.angularBindingProvider();
 
