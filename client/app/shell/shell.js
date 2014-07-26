d27944c7204137d7ddfc5f7fd763fa55a7e104e4
Tyrsius <tyrsius@gmail.com>
2014-07-26 16:57:56 -0700

    add error router

--- client/app/shell/shell.js
+++ client/app/shell/shell.js
@@ -9,7 +9,9 @@ function (router, ko, app) {
 				{ route: '', moduleId: 'contacts/list', title: 'Contacts', nav: true },
 				{ route: 'contacts/new', moduleId: 'contacts/edit', title: 'New Contact', nav: true },
 				{ route: 'contacts/:id', moduleId: 'contacts/edit', title: 'Contact Details', nav: false }
-			]).buildNavigationModel();
+			])
+			.buildNavigationModel()
+			.mapUnknownRoutes('shell/error', 'not-found');
 
 			return router.activate();
 		}
