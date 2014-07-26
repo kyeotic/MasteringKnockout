da794c0cdedbaeab30ad382765fc439093b9b4c3
Tyrsius <tyrsius@gmail.com>
2014-07-26 13:54:23 -0700

    add sample

--- client/app/shell/shell.js
+++ client/app/shell/shell.js
@@ -1,11 +1,9 @@
 define(['knockout', 'durandal/app', 'contacts/edit', 'contacts/list'], 
 function (ko, app, EditVm, ListVm) {
-	var listVm = new ListVm(),
-		editVm = new EditVm();
 	return {
 		title: app.title,
-		currentModel: ko.observable(listVm),
-		setEdit: function() { this.currentModel(editVm); },
-		setList: function() { this.currentModel(listVm); }
+		currentModel: ko.observable(ListVm),
+		setEdit: function() { this.currentModel(EditVm); },
+		setList: function() { this.currentModel(ListVm); }
 	};
 });
