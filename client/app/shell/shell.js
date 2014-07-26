d50062cb8f91aad0f4ba6afc293e79391e3c7204
Tyrsius <tyrsius@gmail.com>
2014-07-26 13:12:10 -0700

    removing extra shell deps

--- client/app/shell/shell.js
+++ client/app/shell/shell.js
@@ -1,10 +1,9 @@
-define(['plugins/router', 'knockout', 'durandal/app', 'contacts/edit', 'contacts/list'], 
+define(['knockout', 'durandal/app', 'contacts/edit', 'contacts/list'], 
 function (router, ko, app, EditVm, ListVm) {
 	var listVm = new ListVm(),
 		editVm = new EditVm();
 	return {
 		title: app.title,
-		router: router,
 		currentModel: ko.observable(listVm),
 		setEdit: function() { this.currentModel(editVm); },
 		setList: function() { this.currentModel(listVm); }
