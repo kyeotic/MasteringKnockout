ff52a1759066cdedf7291f423ee62f918e0bde09
Tyrsius <tyrsius@gmail.com>
2014-08-13 19:55:15 -0700

    adding sample

--- client/app/contact-list.js
+++ client/app/contact-list.js
@@ -1,6 +1,5 @@
-define(['knockout', 'text!contact-list.html'], function(ko, templateString) {
-
-	function ListViewmodel(params) {
+define(['knockout'], function(ko) {
+	return function ListViewmodel(params) {
 		this.contacts = params.contacts;
 		this.edit = params.edit;
 		this.delete = function(contact) {
@@ -9,7 +8,5 @@ define(['knockout', 'text!contact-list.html'], function(ko, templateString) {
 		this.dispose = function() {
 			console.log('List Component Disposing');
 		};
-	}
-
-	return { template: templateString, viewModel: ListViewmodel };
+	};
 });
--- client/app/main.js
+++ client/app/main.js
@@ -15,7 +15,28 @@ require.config({
 
 define(['jquery', 'knockout', 'contactsPage', 'bootstrap'], function($, ko, ContactsPageViewmodel) {
 
-    ko.components.register('contact-list', { require: 'contact-list' });
+    ko.components.register('contact-list', {
+    	template: { fromUrl: 'contact-list.html', maxCacheAge: 100 },
+    	viewModel: { require: 'contact-list'  }
+    });
+
+    var templateFromUrlLoader = {
+	    loadTemplate: function(name, templateConfig, callback) {
+	        if (templateConfig.fromUrl) {
+	            // Uses jQuery's ajax facility to load the markup from a file
+	            var fullUrl = '/app/' + templateConfig.fromUrl + '?cacheAge=' + templateConfig.maxCacheAge;
+	            $.get(fullUrl, function(markupString) {
+	            	callback($.parseHTML(markupString));
+	            });
+	        } else {
+	            // Unrecognized config format. Let another loader handle it.
+	            callback(null);
+	        }
+	    }
+	};
+	 
+	// Register it
+	ko.components.loaders.unshift(templateFromUrlLoader);
 
     $(document).ready(function() {
         ko.applyBindings(new ContactsPageViewmodel());
