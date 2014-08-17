996c198f2eaa0ed1a64ff195e1d448a7fc4c3017
Tyrsius <tyrsius@gmail.com>
2014-08-17 13:23:17 -0700

    fixing nav bar

--- client/app/main.js
+++ client/app/main.js
@@ -17,7 +17,7 @@ require.config({
     waitSeconds: 30
 });
 
-define(['durandal/system', 'durandal/app', 'common/extensions'],
+define(['durandal/system', 'durandal/app', 'common/extensions', 'bootstrap',
 function(system, app, extensions) {
 
     //>>excludeStart("build", true);
