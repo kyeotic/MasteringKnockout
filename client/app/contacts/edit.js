13ee94e2b71e80d8c9c212a958dd5c51375c04d8
Tyrsius <tyrsius@gmail.com>
2014-08-22 17:14:02 -0700

    adding call:

--- client/app/contacts/edit.js
+++ client/app/contacts/edit.js
@@ -22,7 +22,7 @@ function(app, ko, router, dataService, Contact, dialog) {
 		};
 
 		self.show = function() {
-			return dialog.show(self);
+			return dialog.show(self, null, 'bootstrap');
 		};
 
 		self.cancel = function() {
