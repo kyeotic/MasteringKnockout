0ca086174a543751da2cb9631eb212cdadcd0b70
Tyrsius <tyrsius@gmail.com>
2014-09-06 14:39:28 -0700

    finishing sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -5,18 +5,22 @@
                                       {name: "Tiancum", age: 43},
                                       {name: "Jacob", age: 27},
                                       {name: "Nephi", age: 29},
-                                      {name: "Jacob", age: 27},
-                                      {name: "Nephi", age: 29},
-                                      {name: "Jacob", age: 27},
-                                      {name: "Nephi", age: 29},
-                                      {name: "Jacob", age: 27},
-                                      {name: "Nephi", age: 29},
-                                      {name: "Jacob", age: 27},
-                                      {name: "Nephi", age: 29},
-                                      {name: "Jacob", age: 27},
-                                      {name: "Nephi", age: 29},
+                                      {name: "Alex", age: 27},
+                                      {name: "Joseph", age: 29},
+                                      {name: "Meriki", age: 27},
+                                      {name: "Alfred", age: 29},
+                                      {name: "Amanos", age: 27},
+                                      {name: "Kreda", age: 29},
+                                      {name: "Maximillius", age: 27},
+                                      {name: "Wuino", age: 29},
+                                      {name: "Phereas", age: 27},
+                                      {name: "Hector", age: 29},
                                       {name: "Enos", age: 34}])
-	}
+	};
+
+	vm.remove = function(person) {
+		this.people.remove(person);
+	}.bind(vm);
 	
 	$(document).ready(function() {
 		ko.applyBindings(vm);
