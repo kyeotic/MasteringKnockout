9894fa47b1bb8e204ccea72196a9e41a14819936
Tyrsius <tyrsius@gmail.com>
2014-09-06 13:30:34 -0700

    updating sample

--- client/app/sample.js
+++ client/app/sample.js
@@ -5,6 +5,16 @@
                                       {name: "Tiancum", age: 43},
                                       {name: "Jacob", age: 27},
                                       {name: "Nephi", age: 29},
+                                      {name: "Jacob", age: 27},
+                                      {name: "Nephi", age: 29},
+                                      {name: "Jacob", age: 27},
+                                      {name: "Nephi", age: 29},
+                                      {name: "Jacob", age: 27},
+                                      {name: "Nephi", age: 29},
+                                      {name: "Jacob", age: 27},
+                                      {name: "Nephi", age: 29},
+                                      {name: "Jacob", age: 27},
+                                      {name: "Nephi", age: 29},
                                       {name: "Enos", age: 34}])
 	}
 	
