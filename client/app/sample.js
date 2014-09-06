9a087fbdcb8efa4b6d02c7b7ff8533ad8fd90abd
Tyrsius <tyrsius@gmail.com>
2014-09-06 15:09:07 -0700

    adding alerts

--- client/app/sample.js
+++ client/app/sample.js
@@ -1,17 +1,25 @@
 (function(app, $, ko) {
-
-	var Viewmodel = function() {
-		var self = this;
-		self.tooltipText = ko.observable('Tooltip Text');
-		self.progressVal = ko.observable(20);
-
-		self.progressWidth = ko.computed(function(){
-	        return self.progressVal() + '%';
-	    });
-	}
 	
 	$(document).ready(function() {
-		ko.applyBindings(new Viewmodel());
+		var vm = { 
+			tooltipText: ko.observable('Tooltip Text'),
+			alerts: ko.observableArray([
+                        {'message': 'Here is an Error', 'priority': 'danger'},
+                        {'message': 'Here is a Warning', 'priority': 'warning'},
+                        {'message': 'Here is a Success', 'priority': 'success'},
+                        {'message': 'Here is some Info', 'priority': 'info'}
+                    ])
+		};
+
+		vm.addAlert = function() {
+			vm.alerts.push({'message': 'Here is an Error', 'priority': 'danger'});
+		};
+
+		vm.removeAlert = function() {
+			vm.alerts.shift();
+		};
+
+		ko.applyBindings(vm);
 	});
 
 })(window.app = window.app || {}, jQuery, ko);
