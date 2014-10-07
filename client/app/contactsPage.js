65dafd500a76ed393d2506f0e1db2ec6ba260a91
Tyrsius <tyrsius@gmail.com>
2014-10-07 16:45:58 -0700

    fix registration being inside the ctor

--- client/app/contactsPage.js
+++ client/app/contactsPage.js
@@ -1,4 +1,13 @@
 define(['knockout', 'contact', 'dataService'], function(ko, Contact, dataService) {
+
+	var template = document.getElementById('contact-list-template');
+
+	//List Component
+	ko.components.register('contact-list', {
+		template: template.content
+	});
+
+
 	return function ContactsPageViewmodel() {
 		var self = this;
 
@@ -9,13 +18,6 @@ define(['knockout', 'contact', 'dataService'], function(ko, Contact, dataService
 		//Since the contacts array is a function, it can be used as a callback
 		dataService.getContacts(function(contacts) {
 			self.contacts(contacts);
-		});
-
-		var template = document.getElementById('contact-list-template');
-
-		//List Component
-		ko.components.register('contact-list', {
-			template: template.content
 		});
 
 		//
